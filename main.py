fae153cce04e71faa6fc8086f6bffcadfaa83415
SnehaBiswas <>
2021-12-01 12:41:13 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+# print("Welcome to the Band Name Generator.")
+# street = input("What's name of the city you grew up in?\n")
+# pet = input("What's your pet's name?\n")
+# print("Your band name could be " + street + " " + pet)
+print("WELCOME TO BAND NAME GENRATOR\n")
+street=input("Name the city you grew up in:\n")
+pet=input("Name your childhood pet:\n")
+print("Your Band Name could be:"+street+" "+pet)
